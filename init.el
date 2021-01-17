fa032046f334dee121c464b40112dd4bb93456d9
Joel Brewster <hi@joelbrewster.com>
2021-01-18 00:01:39 +1100

    changed to use hosts in straight setup

--- init.el
+++ init.el
@@ -23,7 +23,14 @@
 (setq user-full-name "Joel Brewster"
       user-mail-address "hi@joelbrewster.com")
 
-(use-package aggressive-indent)
+(use-package aggressive-indent
+  :straight
+  (aggressive-indent
+   :type git
+   :host github
+   :repo "Malabarba/aggressive-indent-mode")
+  :config
+  (global-aggressive-indent-mode 1))
 
 (use-package anzu
   :config
@@ -32,16 +39,24 @@
   (global-anzu-mode))
 
 (use-package avy
+  :straight
+  (avy
+   :type git
+   :host github
+   :repo "abo-abo/avy")
   :bind
   (("C-c j" . avy-goto-char-timer)
    ("C-c k" . avy-kill-region)
    ("C-c l" . avy-goto-line)))
 
 (use-package consult
+  :straight
+  (consult
+   :type git
+   :host github
+   :repo "minad/consult")
   :init
-  (fset 'multi-occur #'consult-multi-occur)
-  :config
-  (consult-preview-mode +1)
+  (fset 'multi-occur #'consult-multi-occur)  
   :bind
   (( "C-c o" . consult-outline)
    ( "C-x b" . consult-buffer)
@@ -49,6 +64,7 @@
    ( "C-x 5 b" . consult-buffer-other-frame)
    ( "C-x r x" . consult-register)
    ( "C-x r b" . consult-bookmark)
+   ( "M-s r" . consult-ripgrep)
    ( "M-s o" . consult-outline)
    ( "M-s l" . consult-line)
    ( "M-s m" . consult-multi-occur)
@@ -124,81 +140,105 @@
 	      (dired-hide-details-mode)))
 
 (use-package doom-themes
+  :straight
+  (doom-themes
+   :type git
+   :host github
+   :repo "hlissner/emacs-doom-themes")
   :config
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (setq doom-themes-enable-bold t)
+  (setq doom-themes-enable-italic t)
   (load-theme 'doom-monokai-pro t)
   (doom-themes-org-config))
 
 (use-package emmet-mode
+  :straight
+  (emmet-mode
+   :type git
+   :host github
+   :repo "smihica/emmet-mode")
   :config
   (add-hook 'web-mode-hook 'emmet-mode)
   (add-hook 'css-mode-hook 'emmet-mode)
   (add-hook 'js-mode-hook 'emmet-mode))
 
-(use-package git-gutter-fringe
-  :config
-  (global-git-gutter-mode 1)
-  (setq-default fringes-outside-margins t)
-  (setq-default left-fringe-width 6)
-  (setq-default right-fringe-width 8)
-  ;; (fringe-helper-define 'git-gutter-fr:added nil ".")
-  ;; (fringe-helper-define 'git-gutter-fr:deleted nil ".")
-  ;; (fringe-helper-define 'git-gutter-fr:modified nil ".")
-  (define-fringe-bitmap 'git-gutter-fr:added
-  [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
-  nil nil 'center)
-  (define-fringe-bitmap 'git-gutter-fr:modified
-  [224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
-  nil nil 'center)
-  (define-fringe-bitmap 'git-gutter-fr:deleted
-  [0 0 0 0 0 0 0 0 0 0 0 0 0 128 192 224 240 248]
-  nil nil 'center)
-  (setq
-   git-gutter:modified-sign " "
-   git-gutter:added-sign " "
-   git-gutter:deleted-sign " "))
+(if (display-graphic-p)
+    (use-package git-gutter-fringe
+      :straight
+      (git-gutter-fringe
+       :type git
+       :host github
+       :repo "emacsorphanage/git-gutter-fringe")
+      :config
+      (global-git-gutter-mode 1)
+      (setq-default fringes-outside-margins t)
+      (setq-default left-fringe-width 6)
+      (setq-default right-fringe-width 8)
+      (define-fringe-bitmap 'git-gutter-fr:added
+	[224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
+	nil nil 'center)
+      (define-fringe-bitmap 'git-gutter-fr:modified
+	[224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224 224]
+	nil nil 'center)
+      (define-fringe-bitmap 'git-gutter-fr:deleted
+	[0 0 0 0 0 0 0 0 0 0 0 0 0 128 192 224 240 248]
+	nil nil 'center)
+      (setq git-gutter:modified-sign " ")
+      (setq git-gutter:added-sign " ")
+      (setq git-gutter:deleted-sign " ")))
 
 (use-package goto-last-change
+  :straight
+  (goto-last-change
+   :type git
+   :host github
+   :repo "camdez/goto-last-change.el")
   :bind ("C-c :" . goto-last-change))
 
 (use-package lorem-ipsum
+  :straight
+  (lorem-ipsum
+   :type git
+   :host github
+   :repo "jschaf/emacs-lorem-ipsum")
   :config
   (setq lorem-ipsum-sentence-separator " "))
 
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :hook
-  (lsp-mode . lsp-enable-which-key-integration)
-  (lsp-mode . lsp-lens-mode)
-  :init
-  (setq gc-cons-threshold 100000000)
-  (setq read-process-output-max (* 1024 1024 10))
-  (setq lsp-completion-provider :capf)
-  (setq lsp-enable-snippet t)
-  (setq lsp-enable-indentation t)
-  (setq lsp-semantic-highlighting t)
-  (setq lsp-auto-configure t))
-
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode))
-
 (use-package marginalia
+  :straight
+  (marginalia
+   :type git
+   :host github
+   :repo "minad/marginalia")
   :config
   (marginalia-mode +1))
 
 (use-package magit
+  :straight
+  (magit
+   :type git
+   :host github
+   :repo "magit/magit")
   :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
   :commands (magit-status magit-blame magit-log-buffer-file magit-log-all))
 (add-hook 'magit-mode-hook
 	  (lambda ()
 	    (setq left-fringe-width 8)))
-;; (setq-default with-editor-emacsclient-executable "emacsclient")
 
 (use-package minions
+  :straight
+  (minions
+   :type git
+   :host github
+   :repo "tarsius/minions")
   :config (minions-mode 1))
 
 (use-package move-text
+  :straight
+  (move-text
+   :type git
+   :host github
+   :repo "emacsfodder/move-text")
   :config
   (move-text-default-bindings))
 
@@ -213,6 +253,11 @@
   ("C-c C-<" . mc/mark-all-like-this)))
 
 (use-package olivetti
+  :straight
+  (olivetti
+   :type git
+   :host github
+   :repo "rnkn/olivetti")
   :hook
   (window-configuration-change . my/olivetti-mode-maybe)
   :config
@@ -239,13 +284,13 @@
 (defun my/olivetti-mode-maybe (&optional frame)
   "Turn on `olivetti-mode' for lone buffers.
 
-Doesn't count volatile windows unless the major-mode of their associated buffer
-is found in `my/olivetti-whitelist-modes' or is derived from one of them.
-Windows from buffers whose names are found in `my/olivetti-whitelist-buffers'
-are also considered.
+  Doesn't count volatile windows unless the major-mode of their associated buffer
+  is found in `my/olivetti-whitelist-modes' or is derived from one of them.
+  Windows from buffers whose names are found in `my/olivetti-whitelist-buffers'
+  are also considered.
 
-If FRAME shows exactly one window, turn on `olivetti-mode' for that window.
-Otherwise, disable it everywhere."
+  If FRAME shows exactly one window, turn on `olivetti-mode' for that window.
+  Otherwise, disable it everywhere."
   (let* ((whitelist-buffers my/olivetti-whitelist-buffers)
 	 (whitelist-modes my/olivetti-whitelist-modes)
 	 (predicate (lambda (window)
@@ -310,11 +355,11 @@ Otherwise, disable it everywhere."
 	 ((agenda "" ((org-agenda-span 5)(org-deadline-warning-days 7)))
 	  (todo "NEXT"((org-agenda-overriding-header "Next Tasks")))))
 	("h" tags "+@home")
-	("p" tags "+@pop")
-	("c" tags "+@call")
+	("m" tags "+@mac")
+	("p" tags "+@phone")
 	("e" tags "+@errand")
 	("n" "Next Tasks" ((todo "NEXT" ((org-agenda-overriding-header "Next Tasks")(org-agenda-files '("~/org/life.org"))))))
-	("w" "Next Work Tasks" ((todo "NEXT" ((org-agenda-overriding-header "Next Tasks")(org-agenda-files '("~/org/work.org"))))))
+	("wn" "Next Work Tasks" ((todo "NEXT" ((org-agenda-overriding-header "Next Tasks")(org-agenda-files '("~/org/work.org"))))))
 	))
 
 (setq org-agenda-files (list "~/org/inbox.org"
@@ -329,9 +374,6 @@ Otherwise, disable it everywhere."
 	("i" "Inbox" entry (file "~/org/inbox.org")
 	 "* TODO %?\n  %i\n")
 
-	("f" "Elfeed" entry (file "~/org/inbox.org")
-	 "* %a\n%U")
-
 	("l" "Life" entry (file "~/org/life.org")
 	 "* TODO %?\n  %i\n")
 
@@ -343,18 +385,30 @@ Otherwise, disable it everywhere."
 
 	("c" "Contacts" entry (file "~/org/contacts.org")
 	 "* %?\n  %i\n")
-
-	("e" "Email" entry (file "~/org/life.org")
-	 "* TODO %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n%a\n")
 	))
 
-(use-package prettier-js)
+(use-package prettier-js
+  :straight
+  (prettier-js
+   :type git
+   :host github
+   :repo "prettier/prettier-emacs"))
 (add-hook 'js-mode-hook 'prettier-js-mode)
 
 (use-package rainbow-delimiters
+  :straight
+  (rainbow-delimiters
+   :type git
+   :host github
+   :repo "Fanael/rainbow-delimiters")
   :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package selectrum
+  :straight
+  (selectrum
+   :type git
+   :host github
+   :repo "raxod502/selectrum")
   :config
   (setq selectrum-refine-candidates-function #'orderless-filter)
   (setq selectrum-highlight-candidates-function #'orderless-highlight-matches)
@@ -362,14 +416,33 @@ Otherwise, disable it everywhere."
   (global-set-key (kbd "C-x C-z") #'selectrum-repeat))
 
 (use-package selectrum-prescient
+  :straight
+  (selectrum-prescient
+   :type git
+   :host github
+   :repo "raxod502/prescient.el")
   :after selectrum
   :config
   (prescient-persist-mode +1)
   (selectrum-prescient-mode +1))
 
 (use-package which-key
+:straight
+(which-key
+ :type git
+ :host github
+ :repo "justbur/emacs-which-key")
+    :config
+    (which-key-mode))
+
+(use-package wgrep
+  :straight
+  (wgrep
+   :type git
+   :host github
+   :repo "mhayashi1120/Emacs-wgrep")
   :config
-  (which-key-mode))
+  (setq wgrep-auto-save-buffer t))
 
 (defun 32-random-letter-string ()
   (interactive)
